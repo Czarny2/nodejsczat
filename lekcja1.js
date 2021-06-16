218af2a1a872cf636949b6176c9045a4dcb81958
Czarny2 <szy.kowa@wp.pl>
2021-06-16 08:39:42 +0200

    Pierszy commit

---
+++ lekcja1.js
@@ -0,0 +1,6 @@
+
+
+for(let i =0; i < tablica.length;i++)
+{
+    console.log(tablica[i])
+}
---
+++ lekcja2.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const chalk=require('chalk');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Ile metrow? ', (metry) => {
+    metry=Number,(metry)
+    const yardy=metry*1.0939
+
+  console.log(chalk.blue(`Wynik to ${yardy}`));
+
+  rl.close();
+});
